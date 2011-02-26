889a11938280f4568bc555099ed504c8406439d7
Yorhel <git@yorhel.nl>
2011-02-26 09:47:23 +0100

    Wrote simple text input "widget"

--- ui.ml
+++ ui.ml
@@ -11,19 +11,40 @@ let win_rows = ref 0
 let win_cols = ref 0
 
 
-external ui_getinput : unit -> input_t list = "ui_getinput"
-external ui_init : unit -> unit = "ui_init"
-external ui_end : unit -> unit = "ui_end"
-external ui_checksize : bool ref -> int ref -> int ref -> unit = "ui_checksize"
-external ui_global : string -> (string * bool) list -> unit = "ui_global"
-external ui_tab_main : string array -> int -> unit = "ui_tab_main"
-
-
 class virtual tab = object
   method virtual getTitle : string
   method virtual getName : string
   method virtual draw : unit (* must draw between 0 < y < wincols-2 *)
   method virtual handleInput : input_t -> bool
+end
+
+
+external ui_getinput : unit -> input_t list = "ui_getinput"
+external ui_init : unit -> unit = "ui_init"
+external ui_end : unit -> unit = "ui_end"
+external ui_refresh : unit -> unit = "ui_refresh"
+external ui_textinput_get : string -> string = "ui_textinput_get"
+external ui_textinput_set : string -> string ref -> int ref -> unit = "ui_textinput_set"
+external ui_textinput_key : input_t -> string ref -> int ref -> bool = "ui_textinput_key"
+external ui_textinput_draw : (int * int * int) -> string -> int -> unit = "ui_textinput_draw"
+external ui_checksize : bool ref -> int ref -> int ref -> unit = "ui_checksize"
+external ui_global : tab list -> tab -> unit = "ui_global"
+external ui_tab_main : string array -> int -> unit = "ui_tab_main"
+
+
+
+(* minimal text input "widget".
+ * Has to be written entirely in C to get unicode support working.
+ * TODO: history and tab completion support *)
+class textInput = object(self)
+  (* these are used for storage in C, may be garbage in OCaml *)
+  val curpos = ref 0 (* character count, not bytes nor columns *)
+  val str = ref (String.make 32 '\x00') (* wchar_t* in C *)
+
+  method getText = ui_textinput_get !str
+  method setText a = ui_textinput_set a str curpos
+  method draw y x cols = ui_textinput_draw (y, x, cols) !str !curpos
+  method handleInput t = ui_textinput_key t str curpos
 end
 
 
@@ -33,6 +54,7 @@ class main =
 object(self)
   inherit tab
   val log = Array.make (backlog+1) "" (* circular buffer *)
+  val cmd = new textInput;
   val mutable lastlog = 0
   val mutable lastvisible = 0
 
@@ -44,8 +66,13 @@ object(self)
     lastlog <- lastlog + 1;
     log.(lastlog land backlog) <- str
 
+  method private drawCmd =
+    cmd#draw (!win_rows-3) 0 !win_cols
+
   method draw =
-    ui_tab_main log lastvisible
+    ui_tab_main log lastvisible;
+    cmd#setText "YOYOチラル盛!";
+    self#drawCmd (* must be last to have the cursor at the correct position *)
 
   method handleInput = function
     | Key 0o522 -> (* page down *)
@@ -55,7 +82,9 @@ object(self)
         lastvisible <- min lastlog (max (lastvisible - !win_rows/2)
           (max (!win_rows - 4) (lastlog - backlog + !win_rows - 4)));
         true
-    | _ -> false
+    | k ->
+        if cmd#handleInput k then self#drawCmd;
+        false
 
 end
 
@@ -70,10 +99,10 @@ object(self)
   method draw =
     ui_checksize win_toosmall win_rows win_cols;
     if not !win_toosmall then begin
-      ui_global seltab#getTitle (List.fold_left (fun p tab ->
-        (tab#getName, tab = seltab) :: p) [] tabs);
+      ui_global tabs seltab;
       seltab#draw
-    end
+    end;
+    ui_refresh ()
 
   method handleInput = function
     | Key 0o632   -> (* screen resize *)
--- ui_c.c
+++ ui_c.c
@@ -8,6 +8,7 @@
 #include <caml/mlvalues.h>
 #include <caml/memory.h>
 #include <caml/alloc.h>
+#include <caml/callback.h>
 #include <caml/fail.h>
 
 // acually the wchar_t variant, but the name and location of this one seems to
@@ -42,12 +43,13 @@ CAMLprim value ui_getinput(value unit) {
   /* Mapping from get_wch() to input_t:
    *  KEY_CODE_YES -> Key char_encoded
    *  KEY_CODE_NO:
-   *    char <= 31 || char == 127 -> Ctrl char
-   *    !'^['                     -> Char char_encoded
-   *    ('^[', !)                 -> Esc ""
-   *    ('^[', !Char)             -> ignore both characters (1)
-   *    ('^[', Char && '[')       -> ignore both characters and the character after that (2)
-   *    ('^[', Char && !'[')      -> Esc next_char_encoded
+   *    char == 127           -> Key KEY_BACKSPACE
+   *    char <= 31            -> Ctrl char
+   *    !'^['                 -> Char char_encoded
+   *    ('^[', !)             -> Esc ""
+   *    ('^[', !Char)         -> ignore both characters (1)
+   *    ('^[', Char && '[')   -> ignore both characters and the character after that (2)
+   *    ('^[', Char && !'[')  -> Esc next_char_encoded
    *
    * 1. this is something like ctrl+alt+X, which we won't use
    * 2. these codes indicate a 'Key' that somehow wasn't captured with
@@ -64,7 +66,12 @@ CAMLprim value ui_getinput(value unit) {
       curignore = 0;
       continue;
     }
-    int type = r == KEY_CODE_YES ? INPT_KEY : c == 27 ? INPT_ESC : c <= 31 || c == 127 ? INPT_CTRL : INPT_CHAR;
+    // backspace is often sent as DEL control character, correct this
+    if(r != KEY_CODE_YES && c == 127) {
+      r = KEY_CODE_YES;
+      c = KEY_BACKSPACE;
+    }
+    int type = r == KEY_CODE_YES ? INPT_KEY : c == 27 ? INPT_ESC : c <= 31 ? INPT_CTRL : INPT_CHAR;
     if(type == INPT_CHAR) {
       if((r = wctomb(encoded, c)) < 0)
         failwith("ui_getinput: Cannot encode character");
@@ -99,7 +106,7 @@ CAMLprim value ui_getinput(value unit) {
 
 
 CAMLprim value ui_init(value unit) {
-  CAMLparam1(unit); 
+  CAMLparam1(unit);
   setlocale(LC_ALL, "");
   initscr();
   raw();
@@ -122,6 +129,130 @@ CAMLprim value ui_end(value unit) {
 
 
 
+CAMLprim value ui_refresh(value unit) {
+  CAMLparam1(unit);
+  refresh();
+  CAMLreturn(Val_unit);
+}
+
+
+
+CAMLprim value ui_textinput_get(value str) {
+  CAMLparam1(str);
+  CAMLlocal1(newstr);
+  wchar_t *wstr = (wchar_t *) String_val(str);
+  int len;
+  if((len = wcstombs(NULL, wstr, 0)) < 0)
+    failwith("ui_textinput_get: Cannot encode string");
+  newstr = caml_alloc_string(len);
+  wcstombs(String_val(newstr), wstr, len);
+  CAMLreturn(newstr);
+}
+
+
+
+CAMLprim value ui_textinput_set(value newstr, value str, value curpos) {
+  CAMLparam3(newstr, str, curpos);
+  CAMLlocal1(dest);
+  char *src = String_val(newstr);
+  int len;
+  if((len = mbstowcs(NULL, src, 0)) < 0)
+    failwith("ui_textinput_set: Cannot decode string");
+  dest = caml_alloc_string(sizeof(wchar_t)*(len+1));
+  src = String_val(newstr);
+  mbstowcs((wchar_t *)String_val(dest), src, len+1);
+  Store_field(str, 0, dest);
+  Store_field(curpos, 0, Val_int(len));
+  CAMLreturn(Val_unit);
+}
+
+
+
+CAMLprim value ui_textinput_draw(value loc, value str, value curpos) {
+  CAMLparam3(loc, str, curpos);
+  // TODO: handle the case when the string does not fit on the allocated columns
+  wchar_t *wstr = (wchar_t *)String_val(str);
+  int y = Long_val(Field(loc, 0));
+  int x = Long_val(Field(loc, 1));
+  int col = Long_val(Field(loc, 2));
+  cchar_t t;
+  memset(&t, 0, sizeof(cchar_t));
+  mvhline(y, x, ' ', col);
+  move(y, x);
+  int pos = 0;
+  int i = 0;
+  while(*wstr) {
+    t.chars[0] = *wstr;
+    add_wch(&t);
+    if(i < Long_val(curpos))
+      pos += wcwidth(*wstr);
+    i++;
+    wstr++;
+  }
+  move(y, x+pos);
+  curs_set(1);
+  refresh();
+  CAMLreturn(Val_unit);
+}
+
+
+
+CAMLprim value ui_textinput_key(value key, value str, value curpos) {
+  CAMLparam3(key, str, curpos);
+  CAMLlocal1(tmp);
+  wchar_t *s = (wchar_t *)String_val(Field(str, 0));
+  int len = wcslen(s);
+  int pos = Int_val(Field(curpos, 0));
+  int handled = 1;
+  if(Tag_val(key) == INPT_KEY) {
+    switch(Int_val(Field(key, 0))) {
+      case KEY_LEFT:  if(pos > 0) pos--; break;
+      case KEY_RIGHT: if(pos < len) pos++; break;
+      case KEY_END:   pos = len; break;
+      case KEY_HOME:  pos = 0; break;
+      case KEY_BACKSPACE:
+        if(pos > 0) {
+          memmove(s+pos-1, s+pos, (len-pos+1)*sizeof(wchar_t));
+          pos--;
+        }
+        break;
+      case KEY_DC:
+        if(pos < len)
+          memmove(s+pos, s+pos+1, (len-pos)*sizeof(wchar_t));
+        break;
+      default:
+        handled = 0;
+    }
+  } else if(Tag_val(key) == INPT_CHAR) {
+    // decode character
+    wchar_t c;
+    char *newchar = String_val(Field(key, 0));
+    int r = mbtowc(&c, newchar, strlen(newchar));
+    if(r < 0)
+      caml_failwith("ui_textinput_key: Invalid character encoding");
+    // make sure we have enough space to store the string
+    int buflen = caml_string_length(Field(str, 0)) / sizeof(wchar_t);
+    if(buflen <= len+1) {
+      tmp = caml_alloc_string((len+1)*sizeof(wchar_t)*2);
+      wcscpy((wchar_t *)String_val(tmp), s);
+      s = (wchar_t *)String_val(tmp);
+      Store_field(str, 0, tmp);
+    }
+    // insert character
+    memmove(s+pos+1, s+pos, (len-pos+1)*sizeof(wchar_t));
+    s[pos] = c;
+    pos++;
+  } else
+    handled = 0;
+  if(handled) {
+    Store_field(curpos, 0, Val_int(pos));
+    CAMLreturn(Val_true);
+  }
+  CAMLreturn(Val_false);
+}
+
+
+
 CAMLprim value ui_checksize(value toosmall, value rows, value cols) {
   CAMLparam3(toosmall, rows, cols);
   getmaxyx(stdscr, winrows, wincols);
@@ -133,16 +264,21 @@ CAMLprim value ui_checksize(value toosmall, value rows, value cols) {
 
 
 
-CAMLprim value ui_global(value title, value tabs) {
-  CAMLparam2(title, tabs);
+CAMLprim value ui_global(value tabs, value seltab) {
+  CAMLparam2(tabs, seltab);
   CAMLlocal1(val);
   int i = 0;
+  int getName = hash_variant("getName");
+
+  // a textinput may override this when it is drawn
+  curs_set(0);
 
   erase();
   // first line
   attron(A_REVERSE);
   mvhline(0, 0, ' ', wincols);
-  mvaddstr(0, 0, String_val(title));
+  val = caml_callback(caml_get_public_method(seltab, hash_variant("getTitle")), Val_unit);
+  mvaddstr(0, 0, String_val(val));
   attroff(A_REVERSE);
 
   // tabs
@@ -154,7 +290,8 @@ CAMLprim value ui_global(value title, value tabs) {
   // - Indicate which tab is selected
   while(tabs != Val_emptylist) {
     printw("%d:", ++i);
-    addstr(String_val(Field(Field(tabs, 0), 0)));
+    val = caml_callback(caml_get_public_method(Field(tabs, 0), getName), Val_unit);
+    addstr(String_val(val));
     tabs = Field(tabs, 1);
   }
   attroff(A_REVERSE);
@@ -206,8 +343,7 @@ CAMLprim value ui_tab_main(value log, value lastvisible) {
     free(buffer);
     cur = (cur-1) & backlog;
   }
-  
-  refresh();
+
   CAMLreturn(Val_unit);
 }
 

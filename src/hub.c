srijan/ncdc

commit e47aa33df6a6748ba07a20be7f3096c8c4b3e20f

Implemented requesting and receiving of active search results
ncdc now listens for UDP messages on active_port.

src/hub.c
@@ -92,6 +92,7 @@ struct hub {
   unsigned char nfo_slots, nfo_h_norm, nfo_h_reg, nfo_h_op;
   guint64 nfo_share;
   guint32 nfo_ip4;
+  unsigned short nfo_port;
   // whether we've fetched the complete user list (and their $MyINFO's)
   gboolean received_first; // true if one precondition for joincomplete is satisfied.
   gboolean joincomplete;
@@ -485,12 +486,13 @@ void hub_opencc(struct hub *hub, struct hub_user *u) {
 
 
 // Send a search request
-// TODO: active searches
 void hub_search(struct hub *hub, struct search_q *q) {
   // ADC
   if(hub->adc) {
     // TODO: use FSCH to only get results from active users when we are passive?
     GString *cmd = adc_generate('B', ADCC_SCH, hub->sid, 0);
+    if(cc_listen)
+      g_string_append_printf(cmd, " TO%"G_GUINT64_FORMAT, hub->id);
     if(q->type == 9) {
       char tth[40] = {};
       base32_encode(q->tth, tth);
@@ -515,10 +517,13 @@ void hub_search(struct hub *hub, struct search_q *q) {
 
   // NMDC
   } else {
+    char *dest = cc_listen
+      ? g_strdup_printf("%s:%d", cc_listen_ip, cc_listen_port)
+      : g_strdup_printf("Hub:%s", hub->nick_hub);
     if(q->type == 9) {
       char tth[40] = {};
       base32_encode(q->tth, tth);
-      net_sendf(hub->net, "$Search Hub:%s F?T?0?9?TTH:%s", hub->nick_hub, tth);
+      net_sendf(hub->net, "$Search %s F?T?0?9?TTH:%s", dest, tth);
     } else {
       char *str = g_strjoinv(" ", q->query);
       char *enc = nmdc_encode_and_escape(hub, str);
@@ -526,10 +531,11 @@ void hub_search(struct hub *hub, struct search_q *q) {
       for(str=enc; *str; str++)
         if(*str == ' ')
           *str = '$';
-      net_sendf(hub->net, "$Search Hub:%s %c?%c?%"G_GUINT64_FORMAT"?%d?%s",
-        hub->nick_hub, q->size ? 'T' : 'F', q->ge ? 'F' : 'T', q->size, q->type, enc);
+      net_sendf(hub->net, "$Search %s %c?%c?%"G_GUINT64_FORMAT"?%d?%s",
+        dest, q->size ? 'T' : 'F', q->ge ? 'F' : 'T', q->size, q->type, enc);
       g_free(enc);
     }
+    g_free(dest);
   }
 }
 
@@ -546,6 +552,7 @@ void hub_send_nfo(struct hub *hub) {
   unsigned char slots, h_norm, h_reg, h_op;
   guint64 share;
   guint32 ip4;
+  unsigned short port;
 
   desc = conf_hub_get(string, hub->tab->name, "description");
   conn = conf_hub_get(string, hub->tab->name, "connection");
@@ -572,11 +579,12 @@ void hub_send_nfo(struct hub *hub) {
   }
   slots = conf_slots();
   ip4 = cc_listen ? ip4_pack(cc_listen_ip) : 0;
+  port = cc_listen ? cc_listen_port : 0;
   share = fl_local_list_size;
 
   // check whether we need to make any further effort
-  if(hub->nick_valid && streq(desc) && streq(conn) && streq(mail)
-      && eq(slots) && eq(h_norm) && eq(h_reg) && eq(h_op) && eq(share) && eq(ip4)) {
+  if(hub->nick_valid && streq(desc) && streq(conn) && streq(mail) && eq(slots)
+      && eq(h_norm) && eq(h_reg) && eq(h_op) && eq(share) && eq(ip4) && eq(port)) {
     g_free(desc);
     g_free(conn);
     g_free(mail);
@@ -598,9 +606,14 @@ void hub_send_nfo(struct hub *hub) {
     }
     if(f || !eq(ip4)) {
       g_string_append_printf(cmd, " I4%s", ip4_unpack(ip4)); // ip4 = 0 == 0.0.0.0, which is exactly what we want
-      g_string_append(cmd, ip4 ? " SUTCP4" : " SU");
+      g_string_append(cmd, ip4 ? " SUTCP4,UDP4" : " SU");
     }
-    if(f || !eq(share))
+    if((f || !eq(port))) {
+      if(port)
+        g_string_append_printf(cmd, " U4%d", port);
+      else
+        g_string_append(cmd, " U4");
+    } if(f || !eq(share))
       g_string_append_printf(cmd, " SS%"G_GUINT64_FORMAT, share);
     if(f || !eq(slots))
       g_string_append_printf(cmd, " SL%d", slots);

src/cc.c
@@ -1056,12 +1056,14 @@ void cc_free(struct cc *cc) {
 
 // Active mode
 
-// listen socket. NULL if we aren't active.
-GSocket *cc_listen = NULL;
-char    *cc_listen_ip = NULL; // human-readable string. This is the remote IP, not the one we bind to.
+
+GSocket *cc_listen = NULL;     // TCP listen socket. NULL if we aren't active.
+GSocket *cc_listen_udp = NULL; // UDP listen socket.
+char    *cc_listen_ip = NULL;  // human-readable string. This is the remote IP, not the one we bind to.
 guint16  cc_listen_port = 0;
 
-static int cc_listen_src = 0;
+static int cc_listen_tcp_src = 0;
+static int cc_listen_udp_src = 0;
 
 
 static void cc_listen_stop() {
@@ -1069,26 +1071,144 @@ static void cc_listen_stop() {
     return;
   g_free(cc_listen_ip);
   cc_listen_ip = NULL;
-  g_source_remove(cc_listen_src);
+  g_source_remove(cc_listen_tcp_src);
+  g_source_remove(cc_listen_udp_src);
   g_object_unref(cc_listen);
-  cc_listen = FALSE;
+  g_object_unref(cc_listen_udp);
+  cc_listen = cc_listen_udp = FALSE;
 }
 
 
-static gboolean listen_handle(GSocket *sock, GIOCondition cond, gpointer dat) {
+static gboolean listen_tcp_handle(GSocket *sock, GIOCondition cond, gpointer dat) {
   GError *err = NULL;
   GSocket *s = g_socket_accept(sock, NULL, &err);
   if(!s) {
     if(err->code != G_IO_ERROR_WOULD_BLOCK) {
       ui_mf(ui_main, 0, "Listen error: %s. Switching to passive mode.", err->message);
-      hub_global_nfochange();
       cc_listen_stop();
+      hub_global_nfochange();
     }
     g_error_free(err);
     return FALSE;
   }
   cc_incoming(cc_create(NULL), s);
   return TRUE;
+}
+
+
+static void listen_udp_handle_msg(char *addr, char *msg, gboolean adc) {
+  if(!msg[0])
+    return;
+  struct search_r *r = NULL;
+
+  // ADC
+  if(adc) {
+    GError *err = NULL;
+    struct adc_cmd cmd;
+    adc_parse(msg, &cmd, NULL, &err);
+    if(err) {
+      g_warning("ADC parse error from UDP:%s: %s. --> %s", addr, err->message, msg);
+      g_error_free(err);
+      return;
+    }
+    r = search_parse_adc(NULL, &cmd);
+    g_strfreev(cmd.argv);
+
+  // NMDC
+  } else
+    r = search_parse_nmdc(NULL, msg);
+
+  // Handle search result
+  if(r) {
+    ui_search_global_result(r);
+    search_r_free(r);
+  } else
+    g_warning("Invalid search result from UDP:%s: %s", addr, msg);
+}
+
+
+static gboolean listen_udp_handle(GSocket *sock, GIOCondition cond, gpointer dat) {
+  static char buf[5000]; // can be static, this function is only called in the main thread.
+  GError *err = NULL;
+  GSocketAddress *addr = NULL;
+  int r = g_socket_receive_from(sock, &addr, buf, 5000, NULL, &err);
+
+  // handle error
+  if(r < 0) {
+    if(err->code != G_IO_ERROR_WOULD_BLOCK) {
+      ui_mf(ui_main, 0, "UDP read error: %s. Switching to passive mode.", err->message);
+      cc_listen_stop();
+      hub_global_nfochange();
+    }
+    g_error_free(err);
+    return FALSE;
+  }
+
+  // get source ip:port in a readable fashion, for debugging
+  char addr_str[100];
+  GInetSocketAddress *socka = G_INET_SOCKET_ADDRESS(addr);
+  if(!socka)
+    strcat(addr_str, "(addr error)");
+  else {
+    char *ip = g_inet_address_to_string(g_inet_socket_address_get_address(socka));
+    g_snprintf(addr_str, 100, "%s:%d", ip, g_inet_socket_address_get_port(socka));
+    g_free(ip);
+  }
+  g_object_unref(addr);
+
+  // check for ADC or NMDC
+  gboolean adc = FALSE;
+  if(buf[0] == 'U')
+    adc = TRUE;
+  else if(buf[0] != '$') {
+    g_message("Received invalid message on UDP from %s: %s", addr_str, buf);
+    return TRUE;
+  }
+
+  // handle message. since all we receive is either URES or $SR, we can handle that here
+  char *cur = buf, *next = buf;
+  while((next = strchr(cur, adc ? '\n' : '|')) != NULL) {
+    *(next++) = 0;
+    g_debug("UDP:%s< %s", addr_str, cur);
+    listen_udp_handle_msg(addr_str, cur, adc);
+    cur = next;
+  }
+
+  return TRUE;
+}
+
+
+// TODO: option to bind to a specific IP, for those who want that functionality
+static GSocket *listen_sock_create(int port, gboolean tcp, GError **err) {
+  GError *tmperr = NULL;
+
+  // get local address
+  GInetAddress *laddr = g_inet_address_new_any(G_SOCKET_FAMILY_IPV4);
+  GSocketAddress *saddr = G_SOCKET_ADDRESS(g_inet_socket_address_new(laddr, port));
+  g_object_unref(laddr);
+
+  // create the socket
+  GSocket *s = g_socket_new(G_SOCKET_FAMILY_IPV4,
+    tcp ? G_SOCKET_TYPE_STREAM : G_SOCKET_TYPE_DATAGRAM,
+    tcp ? G_SOCKET_PROTOCOL_TCP : G_SOCKET_PROTOCOL_UDP, NULL);
+  g_socket_set_blocking(s, FALSE);
+
+  // bind to the address
+  g_socket_bind(s, saddr, TRUE, &tmperr);
+  g_object_unref(saddr);
+  if(tmperr) {
+    g_propagate_error(err, tmperr);
+    g_object_unref(s);
+    return NULL;
+  }
+
+  // for TCP: listen()
+  if(tcp && !g_socket_listen(s, &tmperr)) {
+    g_propagate_error(err, tmperr);
+    g_object_unref(s);
+    return NULL;
+  }
+  return s;
 }
 
 
@@ -1105,42 +1225,44 @@ gboolean cc_listen_start() {
   // can be 0, in which case it'll be randomly assigned
   int port = g_key_file_get_integer(conf_file, "global", "active_port", NULL);
 
-  // TODO: option to bind to a specific IP, for those who want that functionality
-  GInetAddress *laddr = g_inet_address_new_any(G_SOCKET_FAMILY_IPV4);
-  GSocketAddress *saddr = G_SOCKET_ADDRESS(g_inet_socket_address_new(laddr, port));
-  g_object_unref(laddr);
-
-  // create(), bind() and listen()
-  GSocket *s = g_socket_new(G_SOCKET_FAMILY_IPV4, G_SOCKET_TYPE_STREAM, G_SOCKET_PROTOCOL_TCP, NULL);
-  g_socket_set_blocking(s, FALSE);
-  g_socket_bind(s, saddr, TRUE, &err);
-  g_object_unref(saddr);
-  if(err) {
-    ui_mf(ui_main, 0, "Error creating listen socket: %s", err->message);
+  // Open TCP listen socket
+  GSocket *tcp = listen_sock_create(port, TRUE, &err);
+  if(!tcp) {
+    ui_mf(ui_main, 0, "Error creating TCP listen socket: %s", err->message);
     g_error_free(err);
-    g_object_unref(s);
     return FALSE;
   }
-  if(!g_socket_listen(s, &err)) {
-    ui_mf(ui_main, 0, "Error creating listen socket: %s", err->message);
+
+  // The port could have changed (in case it was 0), so re-fetch it and use
+  // that for the UDP port as well.
+  GSocketAddress *addr = g_socket_get_local_address(tcp, NULL);
+  port = g_inet_socket_address_get_port(G_INET_SOCKET_ADDRESS(addr));
+  g_object_unref(addr);
+
+  // Open UDP listen socket
+  GSocket *udp = listen_sock_create(port, FALSE, &err);
+  if(!udp) {
+    ui_mf(ui_main, 0, "Error creating UDP listen socket: %s", err->message);
     g_error_free(err);
-    g_object_unref(s);
     return FALSE;
   }
 
   // attach incoming connections handler to the event loop
-  GSource *src = g_socket_create_source(s, G_IO_IN, NULL);
-  g_source_set_callback(src, (GSourceFunc)listen_handle, NULL, NULL);
-  cc_listen_src = g_source_attach(src, NULL);
+  GSource *src = g_socket_create_source(tcp, G_IO_IN, NULL);
+  g_source_set_callback(src, (GSourceFunc)listen_tcp_handle, NULL, NULL);
+  cc_listen_tcp_src = g_source_attach(src, NULL);
+  g_source_unref(src);
+  // and start receiving incoming UDP messages
+  src = g_socket_create_source(udp, G_IO_IN, NULL);
+  g_source_set_callback(src, (GSourceFunc)listen_udp_handle, NULL, NULL);
+  cc_listen_udp_src = g_source_attach(src, NULL);
   g_source_unref(src);
 
   // set global variables
-  cc_listen = s;
+  cc_listen = tcp;
+  cc_listen_udp = udp;
+  cc_listen_port = port;
   cc_listen_ip = g_key_file_get_string(conf_file, "global", "active_ip", NULL);
-  // actual listen port may be different from what we specified (notably when port = 0)
-  GSocketAddress *addr = g_socket_get_local_address(s, NULL);
-  cc_listen_port = g_inet_socket_address_get_port(G_INET_SOCKET_ADDRESS(addr));
-  g_object_unref(addr);
 
   ui_mf(ui_main, 0, "Listening on port %d (%s).", cc_listen_port, cc_listen_ip);
   hub_global_nfochange();

src/util.c
@@ -1204,7 +1204,6 @@ struct search_r *search_r_copy(struct search_r *r) {
 
 
 // Currently requires hub to be valid. Modifies msg in-place for temporary stuff.
-// TODO: Handle active responses (hub=NULL)
 struct search_r *search_parse_nmdc(struct hub *hub, char *msg) {
   struct search_r r = {};
   char *tmp, *tmp2;
@@ -1224,8 +1223,15 @@ struct search_r *search_parse_nmdc(struct hub *hub, char *msg) {
   // msg is now searched backwards, because we can't reliably determine the end
   // of the filename otherwise.
 
-  // <space>(hub_ip:hub_port). Also ignored since we only support passive results for now.
+  // <space>(hub_ip:hub_port).
   tmp = strrchr(msg, ' ');
+  if(!tmp)
+    return NULL;
+  *(tmp++) = 0;
+  if(*(tmp++) != '(')
+    return NULL;
+  char *hubaddr = tmp;
+  tmp = strchr(tmp, ')');
   if(!tmp)
     return NULL;
   *tmp = 0;
@@ -1272,6 +1278,23 @@ struct search_r *search_parse_nmdc(struct hub *hub, char *msg) {
   while(--tmp > r.file && *tmp == '/')
     *tmp = 0;
 
+  // For active search results: figure out the hub
+  if(!hub) {
+    tmp = strchr(hubaddr, ':') ? g_strdup(hubaddr) : g_strdup_printf("%s:411", hubaddr);
+    GList *n;
+    struct ui_tab *t;
+    for(n=ui_tabs; n; n=n->next) {
+      t = n->data;
+      // TODO: cache net_remoteaddr()? Or even create a lookup table for it?
+      if(t->type == UIT_HUB && t->hub->nick_valid && !t->hub->adc && strcmp(tmp, net_remoteaddr(t->hub->net)) == 0)
+        break;
+    }
+    g_free(tmp);
+    if(!n)
+      return FALSE;
+    hub = t->hub;
+  }
+
   // Figure out r.uid
   struct hub_user *u = g_hash_table_lookup(hub->users, user);
   if(!u)
@@ -1285,25 +1308,32 @@ struct search_r *search_parse_nmdc(struct hub *hub, char *msg) {
 }
 
 
-// TODO: active responses
 struct search_r *search_parse_adc(struct hub *hub, struct adc_cmd *cmd) {
   struct search_r r = {};
   char *tmp, *tmp2;
 
+  // If this came from UDP, fetch the users' CID
+  if(!hub && (cmd->type != 'U' || cmd->argc < 1 || !istth(cmd->argv[0])))
+    return NULL;
+  char cid[24];
+  if(!hub)
+    base32_decode(cmd->argv[0], cid);
+  char **argv = hub ? cmd->argv : cmd->argv+1;
+
   // file
-  r.file = adc_getparam(cmd->argv, "FN", NULL);
+  r.file = adc_getparam(argv, "FN", NULL);
   if(!r.file)
     return NULL;
   while(strlen(r.file) > 1 && r.file[strlen(r.file)-1] == '/')
     r.file[strlen(r.file)-1] = 0;
 
   // tth & size
-  tmp = adc_getparam(cmd->argv, "TR", NULL);
+  tmp = adc_getparam(argv, "TR", NULL);
   if(tmp) {
     if(!istth(tmp))
       return NULL;
     base32_decode(tmp, r.tth);
-    tmp = adc_getparam(cmd->argv, "SI", NULL);
+    tmp = adc_getparam(argv, "SI", NULL);
     if(!tmp)
       return NULL;
     r.size = g_ascii_strtoull(tmp, &tmp2, 10);
@@ -1313,18 +1343,37 @@ struct search_r *search_parse_adc(struct hub *hub, struct adc_cmd *cmd) {
     r.size = G_MAXUINT64;
 
   // slots
-  tmp = adc_getparam(cmd->argv, "SL", NULL);
+  tmp = adc_getparam(argv, "SL", NULL);
   if(tmp) {
     r.slots = g_ascii_strtoull(tmp, &tmp2, 10);
     if(tmp == tmp2 || !tmp2 || *tmp2)
       return NULL;
   }
 
-  // uid
-  struct hub_user *u = g_hash_table_lookup(hub->sessions, GINT_TO_POINTER(cmd->source));
-  if(!u)
-    return NULL;
-  r.uid = u->uid;
+  // uid - passive
+  if(hub) {
+    struct hub_user *u = g_hash_table_lookup(hub->sessions, GINT_TO_POINTER(cmd->source));
+    if(!u)
+      return NULL;
+    r.uid = u->uid;
+
+  // uid - active. Active responses must have the hubid in the token, from
+  // which we can generate the uid.
+  } else {
+    tmp = adc_getparam(argv, "TO", NULL);
+    if(!tmp)
+      return NULL;
+    guint64 hubid = g_ascii_strtoull(tmp, &tmp2, 10);
+    if(tmp == tmp2 || !tmp2 || *tmp2)
+      return NULL;
+    struct tiger_ctx t;
+    tiger_init(&t);
+    tiger_update(&t, (char *)&hubid, 8);
+    tiger_update(&t, cid, 24);
+    char res[24];
+    tiger_final(&t, res);
+    memcpy(&r.uid, res, 8);
+  }
 
   // If we're here, then we can safely copy and return the result.
   return search_r_copy(&r);

commit c1da9bf17d92bf672ae5c75b6044ba316739b252

hub.c: Fixed bug with constantly sending nfo without changes
Broken in: e47aa33df6a6748ba07a20be7f3096c8c4b3e20f

src/hub.c
@@ -656,6 +656,7 @@ void hub_send_nfo(struct hub *hub) {
   hub->nfo_h_op = h_op;
   hub->nfo_share = share;
   hub->nfo_ip4 = ip4;
+  hub->nfo_port = port;
 }
 
 #undef eq
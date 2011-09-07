706ff90e362cf57c4669dcda9f578a0961bbc507
Yorhel <git@yorhel.nl>
2011-09-07 18:36:37 +0200

    Auto-generate doc/ncdc.1 to add documentation on the commands
    And the version string is also updated automatically. Yay!
    
    I have also edited some of the command documentation to be more
    presentable in man page format. This makes it slightly less readable /
    comfortable when used within ncdc, but that can be improved by fixing
    the ignore-space-on-new-line-on-wrap bug.

---
+++ doc/gendoc.c
@@ -0,0 +1,99 @@
+/* ncdc - NCurses Direct Connect client
+
+  Copyright (c) 2011 Yoran Heling
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+
+#define DOC_CMD
+#include "../src/doc.h"
+
+
+static void out_string(const char *str) {
+  const char *m;
+  while((m = strchr(str, '\n')) != NULL) {
+    fwrite(str, 1, m-str, stdout);
+    fputs("\n.br\n", stdout);
+    str = m+1;
+  }
+  fputs(str, stdout);
+}
+
+
+static void gen_cmd() {
+  const struct doc_cmd *c = doc_cmds;
+  for(; *c->name; c++) {
+    printf(".TP\n\\fB/%s\\fP %s\n.br\n", c->name, c->args ? c->args : "");
+    out_string(c->desc ? c->desc : c->sum);
+    printf("\n");
+  }
+}
+
+
+int main(int argc, char **argv) {
+  /*
+  if(argc != 2) {
+    fprintf(stderr, "Invalid argument.");
+    return 1;
+  }
+  if(strcmp(argv[1], "cmd") == 0)
+    gen_cmd();
+  else {
+    fprintf(stderr, "Invalid argument.");
+    return 1;
+  }
+  return 0;
+  */
+
+  if(argc != 1) {
+    fprintf(stderr, "This command does not accept any commandline arguments.");
+    return 1;
+  }
+  char line[4096];
+  while(fgets(line, sizeof(line), stdin) != NULL) {
+    char *t = line;
+    char *m;
+    while((m = strchr(t, '@')) != NULL) {
+      fwrite(t, 1, m-t, stdout);
+      t = m;
+      if(strncmp(m, "@commands@", 10) == 0) {
+        gen_cmd();
+        t += 10;
+      } else if(strncmp(m, "@version@", 9) == 0) {
+        printf("%s", PACKAGE_VERSION);
+        t += 9;
+      } else {
+        fputc('@', stdout);
+        t++;
+      }
+    }
+    fputs(t, stdout);
+  }
+
+  return 0;
+}
+
--- src/doc.h
+++ src/doc.h
@@ -26,57 +26,62 @@
 
 #ifdef DOC_CMD
 
-const struct doc_cmd {
-  char name[16], *args, *sum, *desc;
+static const struct doc_cmd {
+  char const name[16], *args, *sum, *desc;
 } doc_cmds[] = {
 
 { "accept", NULL, "Accept the TLS certificate of a hub.",
-  "This command is used only when the keyprint of the TLS certificate of a hub"
-  " does not match the keyprint stored in the configuration file."
+  "Use this command to accept the TLS certificate of a hub. This command is"
+  " used only in the case the keyprint of the TLS certificate of a hub does not"
+  " match the keyprint stored in the configuration file."
 },
 { "browse", "[[-f] <user>]", "Download and browse someone's file list.",
-  "Without arguments, this opens a new tab where you can browse your own file list.\n"
-  "Note that changes to your list are not immediately visible in the browser."
+  "Without arguments, this opens a new tab where you can browse your own file list."
+  " Note that changes to your list are not immediately visible in the browser."
   " You need to re-open the tab to get the latest version of your list.\n\n"
   "With arguments, the user list of the specified user will be downloaded (if"
   " it has not been downloaded already) and the browse tab will open once it's"
   " complete. The `-f' flag can be used to force the file list to be (re-)downloaded."
 },
 { "clear", NULL, "Clear the display.",
-  "Clears the log displayed on the screen. Does not affect the log files in any way.\n"
-  "Ctrl+l is a shortcut for this command."
+  "Clears the log displayed on the screen. Does not affect the log files in any way."
+  " Ctrl+l is a shortcut for this command."
 },
 { "close", NULL, "Close the current tab.",
-  "When closing a hub tab, you will be disconnected from the hub.\n"
-  "Alt+c is a shortcut for this command."
+  "Close the current tab.\n"
+  "When closing a hub tab, you will be disconnected from the hub."
+  " Alt+c is a shortcut for this command."
 },
 { "connect", "[<address>]", "Connect to a hub.",
-  "If no address is specified, will connect to the hub you last used on the current tab.\n"
-  "The address should be in the form of `dchub://host:port/' or `host:port'.\n"
-  "The `:port' part is in both cases optional and defaults to :411.\n\n"
+  "Initiate a connection with a hub. If no address is specified, will connect"
+  " to the hub you last used on the current tab. The address should be in the"
+  " form of `protocol://host:port/' or `host:port'. The `:port' part is in both"
+  " cases optional and defaults to :411. The following protocols are"
+  " recognized: dchub, nmdc, nmdcs, adc, adcs.\n\n"
   "Note that this command can only be used on hub tabs. If you want to open a new"
   " connection to a hub, you need to use /open first. For example:\n"
   "  /open testhub\n"
   "  /connect dchub://dc.some-test-hub.com/\n"
-  "See `/help open' for more information."
+  "See the /open command for more information."
 },
 { "connections", NULL, "Display the connection list.",
-  "Opens a new tab with the connection list."
+  NULL
 },
 { "disconnect", NULL, "Disconnect from a hub.",
-  "Closes the connection with the hub."
+  NULL
 },
 { "gc", NULL, "Perform some garbage collection.",
-  "Cleans up unused data and reorganizes existing data to allow more efficient storage and usage.\n"
-  "Currently, the only thing being cleaned up is the hashdata.dat file.\n\n"
-  "This command may take some time to complete, and will fully block ncdc while it is running.\n"
-  "You won't have to perform this command very often."
+  "Cleans up unused data and reorganizes existing data to allow more efficient"
+  " storage and usage. Currently, this commands cleans up hashdata.dat and"
+  " dl.dat, removes unused files in inc/ and old files in fl/.\n\n"
+  "This command may take some time to complete, and will fully block ncdc while"
+  " it is running. You won't have to perform this command very often."
 },
 { "grant", "[<user>]", "Grant someone a slot.",
-  "Granting a slot to someone allows the user to download from you even if you have no free slots.\n\n"
-  "The slot will be granted for as long as ncdc stays open. If you restart"
-  " ncdc, the user will have to wait for a regular slot. Unless, of course, you"
-  " /grant a slot again.\n\n"
+  "Grant someone a slot. This allows the user to download from you even if you"
+  " have no free slots.  The slot will be granted for as long as ncdc stays"
+  " open. If you restart ncdc, the user will have to wait for a regular slot."
+  " Unless, of course, you /grant a slot again.\n\n"
   "Note that a granted slot is specific to a single hub. If the user is also"
   " on other hubs, he/she will not be granted a slot on those hubs."
 },
@@ -85,7 +90,8 @@ const struct doc_cmd {
   "Use /help <command> to get information about a particular command."
 },
 { "kick", "<user>", "Kick a user from the hub.",
-  "You need to be an OP to be able to use this command."
+  "Kick a user from the hub. This command only works on NMDC hubs, and you need"
+  " to be an OP to be able to use it."
 },
 { "me", "<message>", "Chat in third person.",
   "This allows you to talk in third person. Most clients will display your message as something like:\n"
@@ -94,8 +100,8 @@ const struct doc_cmd {
   " does not have this feature, and your message will be sent as-is, including the /me."
 },
 { "msg", "<user> [<message>]", "Send a private message.",
-  "Send a private message to a user on the currently opened hub.\n"
-  "When no message is given, the tab will be opened but no message will be sent."
+  "Send a private message to a user on the currently opened hub. When no"
+  " message is given, the tab will be opened but no message will be sent."
 },
 { "nick", "[<nick>]", "Alias for `/set nick'.",
   NULL
@@ -107,12 +113,13 @@ const struct doc_cmd {
   "If you have specified an address or have previously connected to a hub"
   " from a tab with the same name, /open will automatically connect to"
   " the hub. Use the `-n' flag to disable this behaviour.\n\n"
-  "See `/help connect' for more information on connecting to a hub."
+  "See /connect for more information on connecting to a hub."
 },
 { "password", "<password>", "Send your password to the hub.",
-  "The /password command can be used to send a password to the hub without saving it to the config file.\n"
-  "If you wish to login automatically without having to type /password every time, use '/set password <password>'."
-  " Be warned, however, that your password will be saved unencrypted in this case."
+  "This command can be used to send a password to the hub without saving it to"
+  " the config file. If you wish to login automatically without having to type"
+  " /password every time, use '/set password <password>'. Be warned, however,"
+  " that your password will be saved unencrypted in this case."
 },
 { "pm", "<user> [<message>]", "Alias for /msg",
   NULL
@@ -121,22 +128,25 @@ const struct doc_cmd {
   NULL
 },
 { "quit", NULL, "Quit ncdc.",
-  "You can also just hit ctrl+c, which is equivalent."
+  "Quit ncdc. You can also just hit ctrl+c, which is equivalent."
 },
 { "reconnect", NULL, "Shortcut for /disconnect and /connect",
-  "When your nick or the hub encoding have been changed, the new settings will be used after the reconnect."
+  "Reconnect to the hub. When your nick or the hub encoding have been changed,"
+  " the new settings will be used after the reconnect."
 },
 { "refresh", "[<path>]", "Refresh file list.",
-  "Initiates a refresh. If no argument is given, the complete list will be refreshed."
-  " Otherwise only the specified directory will be refreshed.\n\n"
-  "The path argument can be either an absolute filesystem path or a virtual path within your share."
+  "Initiates a refresh. If no argument is given, the complete list will be"
+  " refreshed. Otherwise only the specified directory will be refreshed. The"
+  " path argument can be either an absolute filesystem path or a virtual path"
+  " within your share."
 },
 { "say", "<message>", "Send a chat message.",
-  "You normally don't have to use the /say command explicitly, any command not staring"
-  " with '/' will automatically imply `/say <command>'. For example, typing `hello.'"
-  " in the command line is equivalent to `/say hello.'.\n\n"
-  "Using the /say command explicitly may be useful to send message starting with '/' to"
-  " the chat, for example `/say /help is what you are looking for'."
+  "Sends a chat message to the current hub or user. You normally don't have to"
+  " use the /say command explicitly, any command not staring with '/' will"
+  " automatically imply `/say <command>'. For example, typing `hello.' in the"
+  " command line is equivalent to `/say hello.'. Using the /say command"
+  " explicitly may be useful to send message starting with '/' to the chat, for"
+  " example `/say /help is what you are looking for'."
 },
 { "search", "[options] <query>", "Search for files.",
   "Performs a file search, opening a new tab with the results.\n\n"
@@ -157,32 +167,31 @@ const struct doc_cmd {
   "  6  img      Image files.\n"
   "  7  video    Videos files.\n"
   "  8  dir      Directories.\n"
-  "Note that file type matching is done using the file extension, and is not very reliable."
+  "Note that file type matching is done using file extensions, and is not very reliable."
 },
 { "set", "[<key> [<value>]]", "Get or set configuration variables.",
-  "Use /set without arguments to get a list of configuration variables.\n"
-  "/set <key> without value will print out the current value."
+  NULL
 },
 { "share", "[<name> <path>]", "Add a directory to your share.",
   "Use /share without arguments to get a list of shared directories.\n"
-  "When called with a name and a path, the path will be added to your share.\n"
-  "Note that shell escaping may be used in the name. For example, to add a"
+  "When called with a name and a path, the path will be added to your share."
+  " Note that shell escaping may be used in the name. For example, to add a"
   " directory with the name `Fun Stuff', you could do the following:\n"
   "  /share \"Fun Stuff\" /path/to/fun/stuff\n"
   "Or:\n"
   "  /share Fun\\ Stuff /path/to/fun/stuff\n\n"
-  "The full path to the directory will not be visible to others, only the name you give it will be public.\n"
-  "An initial `/refresh' is done automatically on the added directory."
+  "The full path to the directory will not be visible to others, only the name"
+  " you give it will be public. An initial `/refresh' is done automatically on"
+  " the added directory."
 },
-{ "unset", "<key>", "Unset a configuration variable.",
-  "This command will reset the variable back to its default value."
+{ "unset", "[<key>]", "Unset a configuration variable.",
+  "This command can be used to reset a configuration variable back to its default value."
 },
 { "unshare", "[<name>]", "Remove a directory from your share.",
-  "Use /unshare without arguments to get a list of shared directories.\n"
-  "To remove a single directory from your share, use `/unshare <name>'.\n"
-  "To remove all directories from your share, use `/unshare /'.\n\n"
-  "Note: All hash data for the removed directories will be thrown away. All"
-  " files will be re-hashed again when the directory is later re-added."
+  "To remove a single directory from your share, use `/unshare <name>', to"
+  " remove all directories from your share, use `/unshare /'.\n\n"
+  "Note that all hash data for the removed directories will be thrown away. All"
+  " files will have to be re-hashed again when the directory is later re-added."
 },
 { "userlist", NULL, "Open the user list.",
   "Opens the user list of the currently selected hub. Can also be accessed using Alt+u."

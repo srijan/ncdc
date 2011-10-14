d89a5e5ca5d5518b1230be117fb26e2931f5ba7c
Yorhel <git@yorhel.nl>
2011-10-14 18:06:18 +0200

    ui.c: Use strftime() as fallback when compiling against glib < 2.26.0

--- src/ui.c
+++ src/ui.c
@@ -2473,12 +2473,22 @@ void ui_draw() {
   int xoffset = 0;
   char *tfmt = conf_ui_time_format();
   if(strcmp(tfmt, "-") != 0) {
+#if GLIB_CHECK_VERSION(2,26,0)
     GDateTime *tm = g_date_time_new_now_local();
     char *ts = g_date_time_format(tm, tfmt);
     mvaddstr(winrows-2, 1, ts);
     xoffset = 2 + str_columns(ts);
     g_free(ts);
     g_date_time_unref(tm);
+#else
+    // Pre-2.6 users will have a possible buffer overflow and a slightly
+    // different formatting function. Just fucking update your system already!
+    time_t tm = time(NULL);
+    char ts[250];
+    strftime(ts, 11, tfmt, localtime(&tm));
+    mvaddstr(winrows-2, 1, ts);
+    xoffset = 2 + str_columns(ts);
+#endif
   }
   g_free(tfmt);
   // tabs

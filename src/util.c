b86ebe2b370677a2b774a3714986677580f1d98b
Yorhel <git@yorhel.nl>
2011-12-02 10:43:05 +0100

    util.c: Set permissions when open()'ing log file
    Using the two-argument open() is a bad idea in combination with O_CREAT.
    The man page even mentions this:
      "This argument must be supplied when O_CREAT is specified in flags"

--- src/util.c
+++ src/util.c
@@ -946,7 +946,7 @@ static void logfile_checkfile(struct logfile *l) {
 
   // if the log hadn't been opened or has been closed earlier, try to open it again
   if(l->file < 0)
-    l->file = open(l->path, O_WRONLY|O_APPEND|O_CREAT);
+    l->file = open(l->path, O_WRONLY|O_APPEND|O_CREAT, 0666);
   if(l->file < 0)
     g_warning("Unable to open log file '%s' for writing: %s", l->path, g_strerror(errno));
 

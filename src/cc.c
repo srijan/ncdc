7c2b49586503438e7da18076d74a6e6c1a4cdac8
Yorhel <git@yorhel.nl>
2012-01-02 10:55:40 +0100

    Copyright year bump

--- src/cc.c
+++ src/cc.c
@@ -1,6 +1,6 @@
 /* ncdc - NCurses Direct Connect client
 
-  Copyright (c) 2011 Yoran Heling
+  Copyright (c) 2011-2012 Yoran Heling
 
   Permission is hereby granted, free of charge, to any person obtaining
   a copy of this software and associated documentation files (the

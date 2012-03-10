srijan/ncdc

commit 8bc7be9b97dfa3742abe7a7460d5709ef5374bd0

commands.c: Don't strip whitespace from /say

src/commands.c
@@ -1263,8 +1263,9 @@ static struct cmd cmds[] = {
 
 void cmd_handle(char *ostr) {
   // special case: ignore empty commands
+  if(strspn(ostr, " \t") == strlen(ostr))
+    return;
   char *str = g_strdup(ostr);
-  g_strstrip(str);
   if(!str || !str[0]) {
     g_free(str);
     return;
@@ -1284,6 +1285,10 @@ void cmd_handle(char *ostr) {
     cmd = str+1;
     args = sep ? sep+1 : "";
   }
+
+  // Strip whitespace around the argument, unless this is the /say command
+  if(strcmp(cmd, "say") != 0)
+    g_strstrip(args);
 
   // execute command when found, generate an error otherwise
   struct cmd *c = getcmd(cmd);

commit 0f8e4039cf8e6c6ae28e009e31ea007e3d50bd49

commands.c: Don't give a static string to g_strstrip()
Even if it's the empty string, it'll still call memmove(), which then
segfaults for everyone but me. Bug introduced in 8bc7be9b9.

src/commands.c
@@ -1283,7 +1283,7 @@ void cmd_handle(char *ostr) {
     if(sep)
       *sep = 0;
     cmd = str+1;
-    args = sep ? sep+1 : "";
+    args = sep ? sep+1 : str+strlen(str);
   }
 
   // Strip whitespace around the argument, unless this is the /say command